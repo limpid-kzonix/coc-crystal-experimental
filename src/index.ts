3bd46769412c001eadc2db8beb995e2865585715
limpid-kzonix <balyszyn@gmail.com>
2020-05-02 22:42:14 +0300

    Update git ignore check build, update locked verison

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// import * as path from "path";
+
+// import { ExtensionContext, LanguageClient, LanguageClientOptions, languages, ServerOptions, services, TransportKind, workspace } from 'coc.nvim'
+// import { Position, SelectionRange } from 'vscode-languageserver-types'
+// import { TextDocumentPositionParams, TextDocument, RequestType } from 'vscode-languageserver-protocol'
+
+export function activate() {
+
+    // Holds the object of workspace configuration 
+    // which can be specified via package.json in JSON Schema (contributes.configuration)
+    // let settings = workspace.settings;
+}
